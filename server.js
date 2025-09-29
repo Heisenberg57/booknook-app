4f4a4de40490d86cd9c31f2539184044252d7c81
Suyog Nimbre <heisenbergnimbre@gmail.com>
2025-09-29 20:33:31 +0530

    Updated Server.js with more routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const pool = require('./db');
 //Middleware
 app.use(express.json());
 
+/////////// All USERS ROUTES/////////////
+
 //get all users
 app.get("/users",async(req,res)=>{
     try{
@@ -30,6 +32,168 @@ app.post("/users",async(req,res)=>{
     }
 });
 
+// Get a user by ID
+app.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
+// Update a user
+app.put("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    const result = await pool.query(
+      "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+      [name, email, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Delete a user.
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
+////////BOOKS ROUTES ////////
+
+
+//get books
+app.get('/books',async(req,res)=>{
+    try{
+    const result = await pool.query('SELECT * FROM books');
+    res.json(result.rows);
+   }catch(err){
+    res.status(500).json({ error: err.message });
+   }
+});
+
+// Get a book by ID
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM books WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Add a book
+app.post("/books", async (req, res) => {
+  try {
+    const { title, author,published_year } = req.body;
+    const result = await pool.query(
+      "INSERT INTO books (title, author,published_year) VALUES ($1, $2, $3) RETURNING *",
+      [title, author, published_year]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
+// Update a book
+app.put("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author ,published_year} = req.body;
+
+    const result = await pool.query(
+      "UPDATE books SET title = $1, author = $2, published_year=$3 WHERE id = $4 RETURNING *",
+      [title, author, published_year, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+
+// Delete a book
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("DELETE FROM books WHERE id = $1 RETURNING *", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.json({ message: "Book deleted successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//get a book review by id
+app.get('/books/:id/reviews',async(req,res)=>{
+    try{
+        const { id } = req.params;
+        const result = await pool.query(
+      'SELECT r.id, r.rating, r.comment, u.name AS reviewer FROM reviews r JOIN users u ON r.user_id = u.id WHERE r.book_id = $1',
+      [id]
+    );
+    res.json(result.rows);
+
+    }
+    catch(err){
+     res.status(500).json({ error: err.message });
+    }
+});
+
+///// BORROW ROUTES ///////
 //borrow a book
 app.post("/borrow",async(req,res)=>{
     try{
@@ -62,6 +226,8 @@ app.get("/borrowed", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+//////// REVIEWS ROUTES /////
 
 //get a review
 app.get("/reviews",async(req,res)=>{
@@ -162,29 +328,7 @@ app.get("/db-check",async(req,res)=>{
     }
 });
 
-app.get('/books',async(req,res)=>{
-    try{
-    const result = await pool.query('SELECT * FROM books');
-    res.json(result.rows);
-   }catch(err){
-    res.status(500).json({ error: err.message });
-   }
-});
 
-app.get('/books/:id/reviews',async(req,res)=>{
-    try{
-        const { id } = req.params;
-        const result = await pool.query(
-      'SELECT r.id, r.rating, r.comment, u.name AS reviewer FROM reviews r JOIN users u ON r.user_id = u.id WHERE r.book_id = $1',
-      [id]
-    );
-    res.json(result.rows);
-
-    }
-    catch(err){
-     res.status(500).json({ error: err.message });
-    }
-});
 
 
   app.listen(3000, () => {
